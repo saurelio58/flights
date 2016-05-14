271cd33fa4f685813bcf3a9ececb9947ac94de70
Steve Aurelio <saurelio58@gmail.com>
2016-05-14 16:22:51 -0500

    initial URL's of UserController working

--- src/main/java/com/cooksys/flights/controllers/UserController.java
+++ src/main/java/com/cooksys/flights/controllers/UserController.java
@@ -1,6 +1,4 @@
-package com.cooksys.fastbook.controllers;
-
-import java.util.List;
+package com.cooksys.flights.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,119 +6,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.cooksys.fastbook.dao.UserDao;
-import com.cooksys.fastbook.models.Friend;
-import com.cooksys.fastbook.models.Group;
-import com.cooksys.fastbook.models.User;
+import com.cooksys.flights.dao.UserDao;
+import com.cooksys.flights.models.User;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserController 
-{
+public class UserController {
 	@Autowired
 	private UserDao userDao;
-	
-	// fastbook/api/users/
-	@RequestMapping(method = RequestMethod.GET)
-	public List<User> index()
-	{
-		return userDao.index();
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public User addUser(@RequestBody User user)
-	{
-		return userDao.add(user);
-	}
-	
-	// fastbook/api/users/{id}
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public User getUser(@PathVariable Integer id)
-	{
-//		<<<<<<<<<<<<<<<<<<<<<<<<<<<<>
-		
-		
-        RestTemplate restTemplate = new RestTemplate();
-        FlightModel fm = restTemplate.getForObject("http://bc-final-webservice/getFlightModel", FlightModel.class);
-		
-		
-		
-//		<<<<<<<<<<<<<<<<<<<<<<<<<<<<>
 
-		return userDao.get(id);
+	// flights/api/users
+	@RequestMapping(method = RequestMethod.POST)
+	public User addUser(@RequestBody User user) {
+		return userDao.addUser(user);
 	}
-	
-	// fastbook/api/users/find/{name}
-	@RequestMapping(value = "/find/{name}", method = RequestMethod.GET)
-	public List<User> queryUsers(@PathVariable String name)
-	{
-		return userDao.queryUsers(name);
+
+	// flights/api/users/{username}
+	@RequestMapping(value = "/{username}", method = RequestMethod.GET)
+	public User getUser(@PathVariable String username) {
+		return userDao.getUser(username);
 	}
-	
-	// fastbook/api/users/{id}/friends
-	@RequestMapping(value = "/{id}/friends", method = RequestMethod.GET)
-	public List<User> getFriends(@PathVariable Integer id)
-	{
-		return userDao.getFriends(id);
-	}
-	
-	// fastbook/api/users/{id}/my_pending_requests
-	@RequestMapping(value = "/{id}/my_pending_requests", method = RequestMethod.GET)
-	public List<Friend> getMyPendingRequests(@PathVariable Integer id)
-	{
-		return userDao.getMyPendingRequests(id);
-	}
-	
-	// fastbook/api/users/{id}/addRequest
-	// Request body contains logged in user
-	@RequestMapping(value = "/{id}/addRequest", method = RequestMethod.PUT)
-	public Friend addFriendRequest(@PathVariable Integer id, @RequestBody User user)
-	{
-		return userDao.addFriendRequest(id, user);
-	}
-	
-	@RequestMapping(value = "/{id}/acceptRequestFromList", method = RequestMethod.PATCH)
-	public List<Friend> acceptFriendRequestFromList(@PathVariable Integer id, @RequestBody Friend friend)
-	{
-		return userDao.acceptFriendRequestFromList(id, friend);
-	}
-	
-	@RequestMapping(value = "/{profileId}/denyRequestFromList/{senderId}", method = RequestMethod.DELETE)
-	public List<Friend> denyFriendRequestFromList(@PathVariable Integer profileId, @PathVariable Integer senderId)
-	{
-		return userDao.denyFriendRequestFromList(profileId, senderId);
-	}
-	
-	// fastbook/api/users/{id}/request/{loggedInid}
-	@RequestMapping(value = "/{profileId}/getRequest/{loggedInid}", method = RequestMethod.GET)
-	public Friend getFriendRelation(@PathVariable Integer profileId, @PathVariable Integer loggedInid)
-	{
-		return userDao.getFriendRelation(profileId, loggedInid);
-	}
-	@RequestMapping(value = "/{id}/acceptRequest", method = RequestMethod.PATCH)
-	public Friend acceptFriendRequest(@PathVariable Integer id, @RequestBody Friend friend)
-	{
-		return userDao.acceptFriendRequest(id, friend);
-	}
-	
-	@RequestMapping(value = "/{profileId}/denyRequest/{loggedInId}", method = RequestMethod.DELETE)
-	public Friend denyFriendRequest(@PathVariable Integer profileId, @PathVariable Integer loggedInId)
-	{
-		return userDao.denyFriendRequest(profileId, loggedInId);
-	}
-	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public User userByEmail(@RequestBody String email)
-	{
-		return userDao.getByEmail(email);
-	}
-	
-	@RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
-	public List<Group> getUsersGroups(@PathVariable Integer id)
-	{
-		return userDao.getUsersGroups(id);
-	}
-	
+
 }
--- src/main/java/com/cooksys/flights/dao/impl/UserDaoImpl.java
+++ src/main/java/com/cooksys/flights/dao/impl/UserDaoImpl.java
@@ -1,5 +1,6 @@
-package com.cooksys.fastbook.dao.impl;
+package com.cooksys.flights.dao.impl;
 
+import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,12 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.cooksys.fastbook.dao.UserDao;
-import com.cooksys.fastbook.models.Friend;
-import com.cooksys.fastbook.models.FriendId;
-import com.cooksys.fastbook.models.Group;
-import com.cooksys.fastbook.models.Like;
-import com.cooksys.fastbook.models.User;
+import com.cooksys.flights.dao.UserDao;
+import com.cooksys.flights.models.User;
 
 @Repository
 @Transactional
@@ -26,20 +23,13 @@ public class UserDaoImpl implements UserDao {
 		return sessionFactory.getCurrentSession();
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public List<User> index() {
-		Session session = getSession();
-		return session.createQuery("from User").list();
-	}
-
-	@Override
-	public User add(User user) {
+	public User addUser(User user) {
 		Session session = getSession();
 
 		// check to see if user exists
-		String tempEmail = user.getEmail();
-		User tempUser = getByEmail(tempEmail);
+		String tempUsername = user.getUsername();
+		User tempUser = getUser(tempUsername);
 
 		if (tempUser != null) {
 			// returning null indicates user already exists
@@ -47,179 +37,16 @@ public class UserDaoImpl implements UserDao {
 		} else {
 			// create new user
 			session.save(user);
-			System.out.println(user.getId());
-			return get(user.getId());
+			System.out.println(user.getUserId());
+			return getUser(user.getUsername());
 		}
 	}
 
 	@Override
-	public User get(Integer id) {
+	public User getUser(String username) {
 		Session session = getSession();
-		return (User) session.createQuery("from User u where u.id = :id").setInteger("id", id)
+		return (User) session.createQuery("from User u where u.username = :username").setString("username", username)
 				.uniqueResult();
-	}
-
-	// Fuzzy query to find a first name or last name that includes
-	// characters from a users input
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<User> queryUsers(String name) {
-		Session session = getSession();
-		name = "%" + name + "%";
-
-		String hql = "from User u " + "where u.firstName like :string "
-				+ "or u.lastName like :string";
-
-		return session.createQuery(hql).setParameter("string", name).list();
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<User> getFriends(Integer id) {
-		Session session = getSession();
-
-		// Find all friend pairings that have been accepted
-		// where one of the userIds matches the id
-		String hql = "from Friend f " + "where (f.userBySentId.id = :id "
-				+ "or f.userByReceivedId.id = :id) " + "and f.status is not false";
-
-		List<Friend> friendPairings = session.createQuery(hql).setParameter("id", id).list();
-
-		List<User> friends = new ArrayList<User>();
-
-		for (Friend f : friendPairings)
-
-		{
-			// If id is sent user add received user
-			if (f.getId().getSentId() == id) {
-				friends.add(f.getUserByReceivedId());
-			}
-
-			// Otherwise add sent user
-			else {
-				friends.add(f.getUserBySentId());
-			}
-		}
-
-		return friends;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Friend> getMyPendingRequests(Integer id) {
-		Session session = getSession();
-
-		// Find all pending friend requests that have
-		// been sent to the user matching userId
-		String hql = "from Friend f " + "where f.userByReceivedId.id = :id "
-				+ "and f.status is false";
-
-		return session.createQuery(hql).setParameter("id", id).list();
-	}
-
-	/**
-	 * Returns the relationship between two users. A null return means that no
-	 * relationship was found. Friend.status == true means they are friends.
-	 * Friend.friendId.sentId == id means the profile sent the request to the
-	 * logged in user. Friend.friendId.receivedId == id means the logged in user
-	 * sent the profile a request.
-	 * 
-	 * @param id
-	 * @param user
-	 * @return
-	 */
-	@Override
-	public Friend getFriendRelation(Integer profileId, Integer loggedInId) {
-		Session session = getSession();
-		User loggedInUser = get(loggedInId);
-		User profileUser = get(profileId);
-
-		String hql = "from Friend f " + "where (f.userBySentId = :loggedInUser "
-				+ "and f.userByReceivedId = :profileUser) " + "or (f.userBySentId = :profileUser "
-				+ "and f.userByReceivedId = :loggedInUser)";
-
-		return (Friend) session.createQuery(hql).setParameter("loggedInUser", loggedInUser)
-				.setParameter("profileUser", profileUser).uniqueResult();
-	}
-
-	@Override
-	public Friend addFriendRequest(Integer profileId, User user) {
-		Session session = getSession();
-
-		User receiver = get(profileId);
-		FriendId friendId = new FriendId(user.getId(), profileId);
-		Friend friend = new Friend();
-		friend.setId(friendId);
-		friend.setUserByReceivedId(receiver);
-		friend.setUserBySentId(user);
-		session.save(friend);
-
-		return getFriendRelation(profileId, user.getId());
-	}
-
-	@Override
-	public List<Friend> acceptFriendRequestFromList(Integer id, Friend friend) {
-		Session session = getSession();
-
-		friend.setStatus(true);
-		session.update(friend);
-
-		return getMyPendingRequests(id);
-	}
-
-	@Override
-	public List<Friend> denyFriendRequestFromList(Integer profileId, Integer senderId) {
-		Session session = getSession();
-
-		Friend friend = getFriendRelation(profileId, senderId);
-
-		session.delete(friend);
-		return getMyPendingRequests(profileId);
-	}
-
-	@Override
-	public Friend acceptFriendRequest(Integer profileId, Friend friend) {
-		Session session = getSession();
-		User loggedInUser;
-
-		if (friend.getId().getSentId() == profileId)
-			loggedInUser = get(friend.getId().getReceivedId());
-		else
-			loggedInUser = get(friend.getId().getSentId());
-
-		friend.setStatus(true);
-		session.update(friend);
-
-		return getFriendRelation(profileId, loggedInUser.getId());
-	}
-
-	@Override
-	public Friend denyFriendRequest(Integer profileId, Integer loggedInId) {
-		Session session = getSession();
-
-		Friend friend = getFriendRelation(profileId, loggedInId);
-
-		session.delete(friend);
-		return getFriendRelation(profileId, loggedInId);
-	}
-
-	@Override
-	public User getByEmail(String email) {
-		Session session = getSession();
-
-		String hql = "from User u " + "where u.email = :email";
-
-		return (User) session.createQuery(hql).setParameter("email", email).uniqueResult();
-	}
-
-	@Override
-	public List<Group> getUsersGroups(Integer id)
-	{
-		Session session = getSession();
-		
-		String hql= "SELECT gu.group FROM GroupUser gu WHERE gu.id.userId= :userId";
-		
-		return session.createQuery(hql).setInteger("userId", id).list();
 	}
 
 }
