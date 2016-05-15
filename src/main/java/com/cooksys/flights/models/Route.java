a9ab69e72e1ae4549502dc8e5bf6e575d54b66bb
Steve Aurelio <saurelio58@gmail.com>
2016-05-15 17:44:22 -0500

    added 'routeComplete'

--- src/main/java/com/cooksys/flights/models/Route.java
+++ src/main/java/com/cooksys/flights/models/Route.java
@@ -4,18 +4,22 @@ import java.util.LinkedList;
 
 public class Route<Flight> {
 
+	private Boolean routeComplete = false;
 	private LinkedList<Flight> flightList = new LinkedList<Flight>();
-
+	
+	public Boolean getRouteComplete() {
+		return routeComplete;
+	}
+	public void setRouteComplete(Boolean routeComplete) {
+		this.routeComplete = routeComplete;
+	}
 	public LinkedList<Flight> getFlightList() {
 		return flightList;
 	}
-
 	public void setFlightList(LinkedList<Flight> flightList) {
 		this.flightList = flightList;
 	}
 
-//	public void addFlight(Flight flight) {
-//		flightList.addFirst(flight);
-//	}
+	
 
 }
